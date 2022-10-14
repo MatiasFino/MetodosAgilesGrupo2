d4e9a149d9ee6839972da19fc8e60b8399446d13
Facundo Rodríguez Simone <frodriguezsimone@alumnos.exa.unicen.edu.ar>
2022-10-14 16:33:55 -0300

    Diseñe el primer diseño de la interfaz

--- SistAyudantes/src/main/java/com/sistema/ayudantes/sistayudantes/InterfaceController.java
+++ SistAyudantes/src/main/java/com/sistema/ayudantes/sistayudantes/InterfaceController.java
@@ -1,14 +1,37 @@
 package com.sistema.ayudantes.sistayudantes;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 
 public class InterfaceController {
-    @FXML
-    private Label welcomeText;
+    @FXML private Button subjectsButton;
+    @FXML private AnchorPane subjectsPanel;
+    @FXML private AnchorPane principalPanel;
+    @FXML private AnchorPane assistantsPanel;
+    public void onExitButtonClicked(MouseEvent event){ //SALE DE LA INTERFAZ
+        Platform.exit();
+        System.exit(0);
+    }
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void onSubjectsButtonClicked(MouseEvent event){ //PASA A LA SECCION DE MATERIAS
+        subjectsPanel.setVisible(true);
+        principalPanel.setVisible(false);
+        assistantsPanel.setVisible(false);
+    }
+
+    public void onBackPrincipalPanelButtonClicked(MouseEvent event){
+        subjectsPanel.setVisible(false);
+        principalPanel.setVisible(true);
+        assistantsPanel.setVisible(false);
+    }
+
+    public void onBackSubjectsPanelButtonClicked(MouseEvent event){
+        subjectsPanel.setVisible(true);
+        principalPanel.setVisible(false);
+        assistantsPanel.setVisible(false);
     }
 }
--- SistAyudantes/src/main/java/com/sistema/ayudantes/sistayudantes/MainApplication.java
+++ SistAyudantes/src/main/java/com/sistema/ayudantes/sistayudantes/MainApplication.java
@@ -11,8 +11,8 @@ public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("interface.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Sistema de Ayudantes Interinos");
         stage.setScene(scene);
         stage.show();
     }
